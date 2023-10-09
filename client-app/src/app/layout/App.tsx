b1c1a517288608031df3d5ff0f4a7617a164789d
RonaNazarov <Ronanazarov@gmail.com>
2023-10-09 10:38:14 +0300

    End of section 8

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,30 +1,26 @@
-import { Fragment, useEffect} from 'react';
-import {Container} from 'semantic-ui-react'; //maybe add later Header, List 
+import { Fragment } from 'react';
+import { Container } from 'semantic-ui-react'; //maybe add later Header, List 
 import NavBar from './NavBar';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponent';
-import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
+import { Outlet, useLocation } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
 
 function App() {
-  const {activityStore}=useStore();
-
-  useEffect(() =>{ // what we want to do when the application loads up
-    activityStore.loadActivities();
-  },[activityStore]) // Dependencies inside an array - only fires once.
-
-
-if(activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
-
+  const location = useLocation();
   return (
     <Fragment>
-      <NavBar/>
-        <Container style={{marginTop: '7em'}}>
-          <ActivityDashboard/>
-        </Container>
-      
+      {location.pathname === '/' ? <HomePage /> : (
+        <>
+          <NavBar />
+          <Container style={{ marginTop: '7em' }}>
+            <Outlet />
+          </Container>
+        </>
+      )}
+
+
     </Fragment>
-    
+
   )
 }
 
