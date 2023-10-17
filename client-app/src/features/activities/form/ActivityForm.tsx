380cb45e89ee953978e645ccdf4ddaf4d7e07996
RonaNazarov <Ronanazarov@gmail.com>
2023-10-18 00:44:39 +0300

    End of section 15

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { Activity } from "../../../app/models/activity";
+import { ActivityFormValues } from "../../../app/models/activity";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { v4 as uuid } from 'uuid'
 import { Formik,Form } from "formik";
@@ -19,19 +19,11 @@ import MyDateInput from "../../../app/common/form/MyDateInput";
 export default observer(function ActivityForm() {
     const { activityStore } = useStore();
     const { createActivity, updateActivity,
-        loading, loadActivity, loadingInitial } = activityStore;
+         loadActivity, loadingInitial } = activityStore;
     const { id } = useParams();
     const navigate = useNavigate();
 
-    const [activity, setActivity] = useState<Activity>({
-        id: '',
-        title: '',
-        category: '',
-        description: '',
-        date: null,
-        city: '',
-        venue: ''
-    });
+    const [activity, setActivity] = useState<ActivityFormValues>(new ActivityFormValues());
 
     const validationSchema = Yup.object({
         title:Yup.string().required('The activity title is required'),
@@ -43,29 +35,29 @@ export default observer(function ActivityForm() {
     })
 
     useEffect(() => {
-        if (id) loadActivity(id).then(activity => setActivity(activity!));
+        if (id) loadActivity(id).then(activity => setActivity(new ActivityFormValues(activity)));
     }, [id, loadActivity]);
 
-    function handleFormSubmit(activity:Activity){
+    /*function handleFormSubmit(activity:ActivityFormValues){
         if(!activity.id){
             activity.id = uuid();
             createActivity(activity).then(()=>navigate(`/activities/${activity.id}`));
         }else{
             updateActivity(activity).then(()=>navigate(`/activities/${activity.id}`));
         }
-    }
+    }*/
 
-    /*function handleSubmit(activity:Activity){
-        if(activity.id.length===0){
+    function handleFormSubmit(activity:ActivityFormValues){
+        if(!activity.id){
             let newActivity={
                 ...activity,
                 id : uuid()
             }
-            createActivity(newActivity).then(()=>navigate(`/activities/${activity.id}`));
+            createActivity(newActivity).then(()=>navigate(`/activities/${newActivity.id}`));
         }else{
             updateActivity(activity).then(()=>navigate(`/activities/${activity.id}`));
         }
-    }*/
+    }
 
 
     if (loadingInitial) return <LoadingComponent content='Loading activity...' />
@@ -95,7 +87,7 @@ export default observer(function ActivityForm() {
                         <MyTextInput placeholder='Venue' name='venue' />
                         <Button 
                             disabled={isSubmitting || !dirty ||!isValid}
-                            loading={loading} 
+                            loading={isSubmitting} 
                             floated="right" 
                             positive type="submit" content='Submit' />
                         <Button as={Link} to='/activities' floated="right" type="submit" content='Cancel' />
