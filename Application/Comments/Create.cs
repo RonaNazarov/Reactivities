a396b44406d9e8339a0820a0d6387b52d28dab70
RonaNazarov <Ronanazarov@gmail.com>
2023-10-28 17:21:40 +0300

    Error in section 19

--- Application/Comments/Create.cs
+++ Application/Comments/Create.cs
@@ -26,22 +26,22 @@ namespace Application.Comments
         }
         public class Handler : IRequestHandler<Command, Result<CommentDto>>
         {
-            private readonly DataContext _contex;
+            private readonly DataContext _context;
             private readonly IMapper _mapper;
             private readonly IUserAccessor _userAccessor;
-            public Handler(DataContext contex, IMapper mapper,IUserAccessor userAccessor)
+            public Handler(DataContext context, IMapper mapper,IUserAccessor userAccessor)
             {
                 _userAccessor = userAccessor;
                 _mapper = mapper;
-                _contex = contex;
+                _context = context;
             }
 
 
             public async Task<Result<CommentDto>> Handle(Command request, CancellationToken cancellationToken)
             {
-                var activity = await _contex.Activities.FindAsync(request.ActivityId);
+                var activity = await _context.Activities.FindAsync(request.ActivityId);
                 if(activity == null) return null;
-                var user = await _contex.Users
+                var user = await _context.Users
                     .Include(p=>p.Photos)
                     .SingleOrDefaultAsync(x=>x.UserName == _userAccessor.GetUsername());
                 var comment = new Comment
@@ -51,7 +51,7 @@ namespace Application.Comments
                     Body = request.Body
                 };
                 activity.Comments.Add(comment);
-                var success = await _contex.SaveChangesAsync() > 0;
+                var success = await _context.SaveChangesAsync() > 0;
                 if(success) return Result<CommentDto>.Success(_mapper.Map<CommentDto>(comment));
                 return Result<CommentDto>.Failure("Failed to add comment");
             }
